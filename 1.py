6b177d9b158bf7e4e9f7c925637095b4c3ba8515
Kallen Haney <thelonelywhitewolf100@gmail.com>
2017-11-17 21:56:18 +0000

    Add files via upload

--- 1.py
+++ 1.py
@@ -9,16 +9,38 @@ def init():
 
 def makeBigBuilding(mc, x, y, z):
 	#Base Foundation
-	mc.setBlocks(x+1,y,z+1,x+21,y,z+21,1)
+	mc.setBlocks(x+1,y,z+1,x+21,y,z+20,1)
 	#Left Wall
 	mc.setBlocks(x+1,y+1,z+1,x+21,y+21,z+1,1)
 	#Right Wall
-	mc.setBlocks(x+1,y+1,z+21,x+21,y+21,z+21,1)
+	mc.setBlocks(x+1,y+1,z+20,x+21,y+21,z+20,1)
 	#Far Wall
 	mc.setBlocks(x+21,y+1,z+1,x+21,y+21,z+20,1)
+	#Front Wall
+	mc.setBlocks(x+1,y+1,z+1,x+1,y+21,z+20,1)
 	#Ceiling
-	mc.setBlocks(x+1,y+21,z+1,x+21,y+21,z+21,1)
-	mc.setBlocks(x+5,y+3,z+1,x+5,y+6,z+1,20)
+	mc.setBlocks(x+1,y+21,z+1,x+21,y+21,z+20,1)
+	#Door
+	mc.setBlocks(x+1,y+1,z+11,x+1,y+3,z+11,51)
+	#Windows
+	mc.setBlocks(x+3,y+3,z+1,x+3,y+18,z+1,20)
+	mc.setBlocks(x+6,y+3,z+1,x+6,y+18,z+1,20)
+	mc.setBlocks(x+9,y+3,z+1,x+9,y+18,z+1,20)
+	mc.setBlocks(x+12,y+3,z+1,x+12,y+18,z+1,20)
+	mc.setBlocks(x+15,y+3,z+1,x+15,y+18,z+1,20)
+	mc.setBlocks(x+18,y+3,z+1,x+18,y+18,z+1,20)
+	mc.setBlocks(x+3,y+3,z+20,x+3,y+18,z+20,20)
+	mc.setBlocks(x+6,y+3,z+20,x+6,y+18,z+20,20)
+	mc.setBlocks(x+9,y+3,z+20,x+9,y+18,z+20,20)
+	mc.setBlocks(x+12,y+3,z+20,x+12,y+18,z+20,20)
+	mc.setBlocks(x+15,y+3,z+20,x+15,y+18,z+20,20)
+	mc.setBlocks(x+18,y+3,z+20,x+18,y+18,z+20,20)
+	mc.setBlocks(x+21,y+3,z+3,x+21,y+18,z+3,20)
+	mc.setBlocks(x+21,y+3,z+6,x+21,y+18,z+6,20)
+	mc.setBlocks(x+21,y+3,z+9,x+21,y+18,z+9,20)
+	mc.setBlocks(x+21,y+3,z+12,x+21,y+18,z+12,20)
+	mc.setBlocks(x+21,y+3,z+15,x+21,y+18,z+15,20)
+	mc.setBlocks(x+21,y+3,z+18,x+21,y+18,z+18,20)
 
 mc = init()
 x,y,z=mc.player.getTilePos()
